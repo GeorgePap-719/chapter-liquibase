d08709e855e3a9ed3d9cf44e782f210b34dc0118
George Papadopoulos <george.719pap@gmail.com>
2025-02-24 12:18:24 +0200

    Initial commit
    Signed-off-by: George Papadopoulos <george.719pap@gmail.com>

---
+++ src/main/kotlin/org/example/chapterliquibase/ChapterLiquibaseApplication.kt
@@ -0,0 +1,11 @@
+package org.example.chapterliquibase
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChapterLiquibaseApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChapterLiquibaseApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/chapterliquibase/ChapterLiquibaseApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.chapterliquibase
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChapterLiquibaseApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
